c28f0bed07d0a8f2378c124b96a1faff8f4082d0
zjubs <zjubs1@gmail.com>
2024-12-14 13:26:37 +0000

    how I solved day14 part2

--- 2024/Day14/Day14.py
+++ 2024/Day14/Day14.py
@@ -48,5 +48,40 @@ for i in quad.values():
 print(part1)
 
 
-     
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Define grid dimensions
+rows, cols = 101,103
+
+for t in range(10000):
+    robots_curr = []
+    for robot in robots:
+        matches = re.findall(pattern, robot)
+        x,y,vx,vy = (int(i) for i in matches)
+
+        xt = x + (vx * t)
+        yt = y + (vy * t)
+
+        x2 = xt % size[0]
+        y2 = yt % size[1]
+        robots_curr.append((x2,y2))
+
+        # Create an empty grid (0 = empty, 1 = marked)
+        grid = np.zeros((rows, cols), dtype=int)
+
+    # Mark the positions
+    ## these are occurences of patterns in grid so we investigate these
+    if (t+1) % 103 == 66 or (t+1) % 101 == 12:
+        for r, c in robots_curr:
+            grid[r][c] = '1'
+
+        # Print the grid row by row
+        plt.figure(figsize=(6, 6))
+        plt.imshow(grid, cmap="Greys", origin="upper")
+        plt.title(f"Grid State {t+1}")
+        plt.axis("off")
+        plt.savefig(f"2024\Day14\pics\grid_state_{t+1}.png", dpi=300)
+        plt.close()
+
+        zz =1
