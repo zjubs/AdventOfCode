372557881f1f7f8424e4631747fea007e2e5c975
zjubs <zjubs1@gmail.com>
2024-12-16 00:09:36 +0000

    update day15 part 2

--- 2024/Day15/Day15part2.py
+++ 2024/Day15/Day15part2.py
@@ -10,36 +10,37 @@ class Box:
         
      # Implementing __hash__ and __eq__ to make Box instances hashable
     def __hash__(self):
-        return hash(self.pos)  # Use the position as the hash
+        return hash(id(self))
+        #return hash(self.pos)  # Use the position as the hash
 
     def __eq__(self, other):
-        return isinstance(other, Box) and self.pos == other.pos
+        return id(self) == id(other)
         
     @staticmethod
     def _add_tuple(tuple1:tuple,tuple2: tuple):
         return tuple(a + b for a, b in zip(tuple1, tuple2))
     
-    # def contains_position(self, position: tuple) -> bool:
-    #     """Check if a given position is part of this box."""
-    #     return position in self.pos
     
-    def contains_any_position(self, positions: set) -> bool:
-        """Check if the box contains any of the specified positions."""
-        return any(pos in self.pos for pos in positions)
+    def contains_any_position(self, position: tuple) -> bool:
+        #Check if the box contains a specified position
+        return any(pos == position for pos in self.pos)
+    
+    def contains_any_position_box(self, pushed_box_pos: set) -> bool:
+        #Check if the box contains any of the specified positions
+        return any(pos == position for pos in self.pos for position in pushed_box_pos)
 
-    def potential_move(self,dir):
+    def potential_move(self,dir: str):
         # get potential postion of box
         move = self.moves[dir]
         return (self._add_tuple(self.pos[0], move),self._add_tuple(self.pos[1], move))
     
-    def move(self,dir):
+    def move(self,dir: str):
         #move box to new position
         self.pos = self.potential_move(dir)
 
-
-
-    
     def push(self,boxes: set ,grid: dict,dir: str):
+        # use try push to check if can push boxes and get depndent boxes
+        # then move each box
         can_push,push_boxes = self.try_push(boxes,grid,dir)
 
         if can_push:
@@ -49,20 +50,15 @@ class Box:
             return True
         else:
             return False
-        
-
-
-
-
     
     def try_push(self,boxes: set ,grid: dict,dir: str):
+        # check if can push a box and get all dependent boxes
         can_push = True
-        new_pos = self.potential_move
-        (dir)
+        new_pos = self.potential_move(dir)
         other_boxes = boxes - {self}
         
         boxes_in_way = set(
-            box for box in other_boxes if box.contains_any_position(new_pos)
+            box for box in other_boxes if box.contains_any_position_box(new_pos)
         )
         all_child_boxes = boxes_in_way
         if grid[new_pos[0]] == '#' or grid[new_pos[1]] == '#':
@@ -75,14 +71,8 @@ class Box:
                     can_push = False
              
         return can_push, all_child_boxes
-    # need to store all boxes pushed if possible and if they can be pushed
-    #if can push then move them all
-    #if can't push then don't move them
-        
 
-
-
-with open('2024/Day15/Day15Sample3.txt', 'r') as f:
+with open('2024/Day15/Day15Input.txt', 'r') as f:
     puzzle = f.read()
 
 g,m = puzzle.split('\n\n')
@@ -94,7 +84,6 @@ instructions = ''.join(m.split('\n'))
 # prep grid
 grid = {}
 boxes = set()
-box_num = 0
 for i,line in enumerate(g):
     for j,chr in enumerate(line):
 
@@ -114,7 +103,6 @@ for i,line in enumerate(g):
             grid[(i,j*2)] = chr
             grid[(i,j*2+1)] = chr
 
-
 def add_tuple(tuple1,tuple2):
     return tuple(a + b for a, b in zip(tuple1, tuple2))
 
@@ -124,7 +112,8 @@ moves = {
     '<': (0,-1),
     'v': (1,0)}
 
-for dir in instructions:
+for num,dir in enumerate(instructions):
+
     proposed = add_tuple(curr,moves[dir])
 
     proposed_boxes = [box for box in boxes if box.contains_any_position(proposed)]
@@ -140,30 +129,10 @@ for dir in instructions:
         if move:
             curr = proposed
 
+part2 = 0
+for box in boxes:
+    coords = box.pos[0]
+    part2 += 100* coords[0] + coords[1]
 
-# box1 = Box((0,1))
-# box2 = Box((3,2))
+print(f'part2:{part2}')
 
-# boxes = set()
-# boxes.add(box1)
-# boxes.add(box2)
-
-# box1.push(boxes,grid,'>')
-
-# box1.move_to('>')
-
-x = 1
-
-
-
-
-
-# x = 1
-
-
-
-# moves = {
-#     '^': (-1,0),
-#     '>': (0,1),
-#     '<': (0,-1),
-#     'v': (1,0)}
