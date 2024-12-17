4066d0c2b3be27d361034e2775082ee117afa826
zjubs <zjubs1@gmail.com>
2024-12-17 17:10:34 +0000

    update day 17

--- 2024/Day17/Day17.py
+++ 2024/Day17/Day17.py
@@ -15,36 +15,40 @@ prog = [*map(int,prog_init.split(':')[1].strip().split(','))]
 opcodes,operands = prog[0::2], prog[1::2]
 
 
+# TODO use match case instead of if
+
 def get_operand_val(operand):
-    if operand in (0,1,2,3):
-        return operand
-    elif operand == 4:
-        return reg['A']
-    elif operand == 5:
-        return reg['B']
-    elif operand == 6:
-        return reg['C']
+    match operand:
+        case x if x in (1, 2, 3):
+            return operand
+        case 4:
+            return reg['A']
+        case 5:
+            return reg['B']
+        case 6:
+            return reg['C']
 
 
 def apply_inst(opcode,operand,combo,i):
-    if opcode == 0: #adv
-        reg['A'] = reg['A'] //(2**combo)
-    elif opcode == 1: #bxl
-        reg['B'] = reg['B'] ^ operand
-    elif opcode == 2: #bxl
-        reg['B'] = combo % 8
-    elif opcode == 3: #jnz
-        if reg['A'] != 0:
-            i = int(operand / 2 - 1)
-            # need to not jump for next instruction()
-    elif opcode == 4: #bxc
-        reg['B'] = reg['B'] ^ reg['C']
-    elif opcode == 5: #out
-        output.append(combo % 8)
-    elif opcode == 6: #bdv
-        reg['B'] = reg['A'] //(2**combo)
-    elif opcode == 7: #bxl
-        reg['C'] = reg['A'] //(2**combo)
+    match opcode:
+        case 0: #adv
+            reg['A'] = reg['A'] //(2**combo)
+        case 1: #bxl
+            reg['B'] = reg['B'] ^ operand
+        case 2: #bxl
+            reg['B'] = combo % 8
+        case 3: #jnz
+            if reg['A'] != 0:
+                i = int(operand / 2 - 1)
+                # need to not jump for next instruction()
+        case 4: #bxc
+            reg['B'] = reg['B'] ^ reg['C']
+        case 5: #out
+            output.append(combo % 8)
+        case 6: #bdv
+            reg['B'] = reg['A'] //(2**combo)
+        case 7: #bxl
+            reg['C'] = reg['A'] //(2**combo)
     return i
 
 output = []
