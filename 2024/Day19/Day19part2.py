5a0f8a2653681a9331372d2ddcb0a555983507aa
zjubs <zjubs1@gmail.com>
2024-12-19 12:31:21 +0000

    clean up solution for 19part2

--- 2024/Day19/Day19part2.py
+++ 2024/Day19/Day19part2.py
@@ -1,12 +1,10 @@
 
-from collections import defaultdict, deque
-
+from collections import defaultdict
 from heapq import heapify, heappop, heappush
 
 # process inputs
 with open('2024/Day19/Day19Input.txt', 'r') as f:
     towels_in,patterns = f.read().split('\n\n')
-
 
 patterns = patterns.split('\n')
 towels = defaultdict(list)
@@ -14,35 +12,8 @@ towels = defaultdict(list)
 for towel in towels_in.split(', '):
     towels[len(towel)].append(towel)
 
-
 def can_use_towel(p,t,position):
     return p[position:(position + len(t))] == t
-
-def bfs(p, towels):
-    visited = set()
-    queue = deque([0])
-
-    while queue:
-        position = queue.popleft()
-
-        if position == len(p):
-            return True
-        
-
-        if position not in visited:
-            visited.add(position)
-
-            for key in towels:
-                if key <= len(p) - position:
-                    for t in towels[key]:
-                        if can_use_towel(p,t,position):
-                            if (position + len(t)) not in visited:
-                                queue.append(position + len(t))
-
-    return False        
-
-# work out all positions we can visit and number of times (pos,score)
-
 
 def bfs2(p, towels):
     visited = defaultdict(int)
@@ -54,8 +25,6 @@ def bfs2(p, towels):
         position = heappop(positions_to_check)
 
         if position == len(p):
-            # get combintaions and return
-            # combinations += 1
             return visited[position]
         
         for key in towels:
@@ -69,16 +38,12 @@ def bfs2(p, towels):
 
     return 0                           
 
+part1 = 0
 part2 = 0
-i =0
 for pattern in patterns:
-    i+=1
-    print(i)
-    if bfs(pattern, towels):
-        part2 += bfs2(pattern, towels)
+    p_result = bfs2(pattern, towels)
+    part1 += bool(p_result)
+    part2 += bfs2(pattern, towels)
 
-print(part2)
-
-x = 1
-
-
+print(f'Part1: {part1}')
+print(f'Part2: {part2}')
