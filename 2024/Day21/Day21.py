e2df534a99504588e3f06b8fd6d4d7150ba62c91
zjubs <zjubs1@gmail.com>
2024-12-21 22:59:57 +0000

    Update day21 to solve both parts

--- 2024/Day21/Day21.py
+++ 2024/Day21/Day21.py
@@ -5,7 +5,7 @@ from functools import cache
 
 @cache
 def all_paths(start, end,invalid):
-    # invalid should be the ref for the empty cell in the ocntrol panel being used
+    # invalid should be the ref for the empty cell in the control panel being used
 
     x1, y1 = start
     x2, y2 = end
@@ -45,10 +45,6 @@ def all_paths(start, end,invalid):
 
     return valid_paths
 
-print(all_paths((0,0),(2,2),(0,1)))
-
-print(all_paths((1,1),(4,1),(0,1)))
-
 
 grid_numeric_rev ={
     (0,0): '7',
@@ -76,74 +72,49 @@ grid_direction_rev ={
 grid_direction_invalid = (0,0)
 grid_direction  = {val: key for key,val in grid_direction_rev.items()}
 
-# remember to start at A
-code =  'A' + '029A'
 
-def process_code(code):
+@cache
+def solve_paths(paths, level,nlevels):
+    curr_iter_buttonslist =[]
+    for path in paths:
+        path_buttons = 0
+        for s1, e1, in zip(path,path[1:]):
+                paths_1 = all_paths(grid_direction[s1], grid_direction[e1],grid_direction_invalid)
+                if level == nlevels:
+                    updated_paths_1 = {t + ('A',) for t in paths_1}
+                    min_next_iter_buttons = min([len(t) for t in updated_paths_1])
+                else:
+                    updated_paths_1 = {('A',) + t + ('A',) for t in paths_1}
+                    min_next_iter_buttons = solve_paths(frozenset(updated_paths_1), level + 1,nlevels)
+
+                path_buttons += min_next_iter_buttons
+            
+        curr_iter_buttonslist.append(path_buttons)
+    buttons = min(curr_iter_buttonslist) 
+                
+    return buttons
+
+
+def process_code(code,nlevels):
     buttons = 0 
     for s,e in zip(code, code[1:]):
-        buttons_this_step = 100
+        
         paths = all_paths(grid_numeric[s], grid_numeric[e],grid_numeric_invalid)
         updated_paths = {('A',) + t + ('A',) for t in paths}
 
-        level1buttonslist =[]
-        for path in updated_paths:
-            level1_buttons = 0
-            for s1, e1, in zip(path,path[1:]):
-                    paths_1 = all_paths(grid_direction[s1], grid_direction[e1],grid_direction_invalid)
-                    updated_paths_1 = {('A',) + t + ('A',) for t in paths_1}
+        buttons += solve_paths(frozenset(updated_paths), 1,nlevels)
 
-                    
-                    level2buttonslist =[]
-                    for path_1 in updated_paths_1:
-                        level2_buttons = 0
-                        for s2, e2, in zip(path_1,path_1[1:]):
-                                paths_2 = all_paths(grid_direction[s2], grid_direction[e2],grid_direction_invalid)
-                                updated_paths_2 = {t + ('A',) for t in paths_2}
-                                min_buttons = min([len(t) for t in updated_paths_2])
-
-                                level2_buttons += min_buttons
-                        level2buttonslist.append(level2_buttons)
-
-                    min_level1_buttons = min(level2buttonslist)
-                    level1_buttons += min_level1_buttons
-            level1buttonslist.append(level1_buttons)
-        buttons += min(level1buttonslist)
     return buttons
-
-print(process_code(code))
-
-x = 1                    
-
 
 with open('2024/Day21/Day21Input.txt', 'r') as f:
     codes = f.read().split('\n')
-part1 = 0 
+
+part1 = 0
+part2 = 0 
 for code in codes:
     val = int(code[:3])
     code =  'A' + code
-    part1 += val * process_code(code)
-print(part1)
-x = 1
-
-#find min for each path
-# sum for each s2,e2
-# find min for each path 1
-
-
-
-
-
-
-#'029A77'
-# directions
-# diresctions
-# directions
-
-# find all combinations at lowest level
-# then find all combinations at next level
-# find all combinations at last level
-
-# robots start pointing at A
-
-# need shortest sequence, but that means findshortest at each level then find shortest way of doing that above.
+    part1 += val * process_code(code,2)
+    part2 += val * process_code(code,25)
+print(f'Part1: {part1}')
+print(f'Part2: {part2}')
