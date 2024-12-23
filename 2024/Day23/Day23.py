11044b13f18a4a54af03321cd4f70cbb833565df
zjubs <zjubs1@gmail.com>
2024-12-23 11:04:53 +0000

    add day 23 part2 solution

--- 2024/Day23/Day23.py
+++ 2024/Day23/Day23.py
@@ -1,29 +1,36 @@
-
 from collections import defaultdict
 
 with open('2024/Day23/Day23Input.txt', 'r') as f:
     edges = f.read().split('\n')
 
-nodes = defaultdict(list)
+nodes = defaultdict(set)
 for edge in edges:
     a,b  = edge.split('-')
-    nodes[a].append(b)
-    nodes[b].append(a)
+    nodes[a].add(b)
+    nodes[b].add(a)
 
 
-t_nodes = [x for x in nodes if x[0] == 't']
-
-combinations = set()
-for t_node in t_nodes:
-    for i in nodes[t_node]:
+combinations = []
+combinations_seen = set()
+combinations_t = []
+for node in nodes:
+    for i in nodes[node]:
         for j in nodes[i]:
-            if t_node in nodes[j]:
-                combinations.add(tuple(sorted((t_node,i,j))))
+            if node in nodes[j]:
+                curr_set = set((node,i,j)) 
+                if curr_set not in combinations_seen:
+                    combinations_seen.add(frozenset(curr_set))
+                    combinations.append(curr_set)
+                    if 't' in (node[0],i[0],j[0]):
+                        combinations_t.append(curr_set)
+
+print(f'Part1: {len(combinations_t)}')
 
 
-print(f'Part1: {len(combinations)}')
+for node in nodes:
+    for combination in combinations:
+        if combination.issubset(nodes[node]):
+            combination.add(node)
 
-
-
-
-x=1 
+largest = sorted(combinations, key = lambda s: len(s))[-1]
+print(f'Part2: {','.join(sorted(list(largest)))}')
