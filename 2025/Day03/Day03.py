d46a9a2c44689dbef7cd6fc8333e37ec9252842d
zjubs <zjubs1@gmail.com>
2025-12-03 06:25:43 +0000

    clean up day3

--- 2025/Day03/Day03.py
+++ 2025/Day03/Day03.py
@@ -4,25 +4,28 @@ with open('2025/Day03/input.txt', 'r') as f:
 
 result = 0 
 lines = puzzle_input.split('\n')
-for line in lines:
-    line_max = ''
-    pos_adj = 0
-    for i in range(12):
-        
-        if (12-i) ==1: 
-            curr =  line[pos_adj:]
-        else:
-            curr = line[pos_adj :-(12-i-1)]
+n_batteries = [2,12] #part1,part2
+for n in n_batteries:
+    for line in lines:
+        line_max = ''
+        start = 0
 
-        curr_max = '0'
+        for i in range(n,0,-1):
+            
+            if i == 1:
+                curr =  line[start:]
+            else:
+                curr = line[start:-(i-1)]
+
+            curr_max = '0'
+            
+            for j,battery in enumerate(curr):
+                if int(battery)> int(curr_max):
+                    curr_max = battery
+                    start_adj = j + 1
+            start += start_adj
+            line_max = line_max + curr_max
         
-        for j,battery in enumerate(curr):
-            if int(battery)> int(curr_max):
-                curr_max = battery
-                adj = j +1
-        pos_adj += adj
-        line_max = line_max + curr_max
-    
-    result += int(line_max)
-    print(line_max)
-print(result)
+        result += int(line_max)
+
+    print(result)
