d5e80817fab56fbc3eb0836e7e2e5167e9666916
zjubs <zjubs1@gmail.com>
2025-12-03 06:26:57 +0000

    clean up day1

--- 2025/Day01/Day01.py
+++ 2025/Day01/Day01.py
@@ -1,33 +1,49 @@
-with open('2025/Day01/input.txt', 'r') as f:
+with open('2025/Day01/input_test.txt', 'r') as f:
     puzzle_input = f.read()
 
-start = 50
-curr = start
-result = 0
-result2 =0
+dial = 50
+result = result2 = 0
 lines = puzzle_input.split('\n')
 for i in lines:
     if i[0] == "R":
 
-        a = (curr + int(i[1:])) // 100
-        new_curr = (curr + int(i[1:]) ) % 100
+        count_zero = (dial + int(i[1:])) // 100
+        new_dial = (dial + int(i[1:]) ) % 100
 
         
     else:
         b = abs(int(i[1:]) // 100)
         c = abs(int(i[1:])) % 100
-        a = b + int(c>=curr) - int(curr==0)
+        count_zero = b + int(c>=dial) - int(dial==0)
 
-        new_curr = (curr - int(i[1:])) %100
+        new_dial = (dial - int(i[1:])) %100
 
-        
-
-    if curr == 0:
+    if dial == 0:
         result += 1
     
-    
-    result2 += a
-    curr = new_curr
+    result2 += count_zero
+    dial = new_dial
 
 print(result)
 print(result2)
+
+dial = 50
+result = result2 = 0
+dirs={"L": -1, "R":1}
+lines = puzzle_input.split('\n')
+for i in lines:
+    dir, dist = dirs[i[0]], int(i[1:])
+    
+    new_dial = (dial + dir*dist ) % 100
+    count_zero = abs((dial + dir*dist) // 100) + (1 if new_dial ==0 and dir == -1 else 0) -(1 if dial == 0 and dir == -1 else 0)
+
+
+    if dial == 0:
+        result += 1
+    
+    result2 += count_zero
+    dial = new_dial
+
+print(result)
+print(result2)
+
